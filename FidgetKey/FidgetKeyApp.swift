6b7479c6574c55de7b8fda6185d1d67627a58fc1
Jun-Katagiri <jun@katagiri.cc>
2026-04-10 14:16:07 +0900

    Initial Commit

---
+++ FidgetKey/FidgetKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FidgetKeyApp.swift
+//  FidgetKey
+//
+//  Created by Jun Katagiri on 2026/04/10.
+//
+
+import SwiftUI
+
+@main
+struct FidgetKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
